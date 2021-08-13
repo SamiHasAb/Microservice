ef195c1d88f6df60380a25c1e771f03d82666aeb
sami <engsamihassan@gmail.com>
2021-08-13 02:06:45 +0200

    configure cloude API Gateway closes #3

--- cloud-gateway/src/main/java/org/study/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/org/study/cloud/gateway/CloudGatewayApplication.java
@@ -1,15 +1,17 @@
-package org.study.department;
+package org.study.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+
 @EnableEurekaClient
-public class DepartmentServiceApplication {
+@SpringBootApplication
+public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DepartmentServiceApplication.class, args);
+
+		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
 }
